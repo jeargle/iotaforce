2bf6b4c8405f0284c79aaefcc911aee1e756be71
John Eargle <jeargle@gmail.com>
2017-01-07 15:36:13 -0600

    added initial files

---
+++ js/iotaforce.js
@@ -0,0 +1,106 @@
+var score, bootState, loadState, titleState, playState, endState, game;
+
+score = 0;
+
+bootState = {
+    create: function() {
+        'use strict';
+
+        // Load physics engine
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.state.start('load');
+    }
+};
+
+loadState = {
+    preload: function() {
+        'use strict';
+        var loadLbl;
+
+        loadLbl = game.add.text(80, 160, 'loading...',
+                                {font: '30px Courier',
+                                 fill: '#ffffff'});
+        
+        // Load images
+
+        // Load sound effects
+    },
+    create: function() {
+        'use strict';
+        game.state.start('title');
+    }
+};
+
+titleState = {
+    create: function() {
+        'use strict';
+        var nameLbl, startLbl, wKey;
+
+        nameLbl = game.add.text(80, 160, 'IOTA FORCE',
+                                {font: '50px Courier',
+                                 fill: '#ffffff'});
+        startLbl = game.add.text(80, 240, 'press "W" to start',
+                                 {font: '30px Courier',
+                                  fill: '#ffffff'});
+
+        wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        wKey.onDown.addOnce(this.start, this);
+    },
+    start: function() {
+        'use strict';
+        game.state.start('play');
+    }
+};
+
+playState = {
+    create: function() {
+        'use strict';
+
+        this.keyboard = game.input.keyboard;
+
+        // Controls
+    },
+    update: function() {
+        'use strict';
+
+    },
+    end: function() {
+        'use strict';
+        game.state.start('end');
+    }
+};
+
+endState = {
+    create: function() {
+        'use strict';
+        var scoreLbl, nameLbl, startLbl, wKey;
+
+        scoreLbl = game.add.text(600, 10, 'Score: ' + score,
+                                 {font: '30px Courier',
+                                  fill: '#ffffff'});
+        nameLbl = game.add.text(80, 160, 'YOU WIN',
+                                {font: '50px Courier',
+                                 fill: '#ffffff'});
+        startLbl = game.add.text(80, 240, 'press "W" to restart',
+                                 {font: '30px Courier',
+                                  fill: '#ffffff'});
+
+        wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        wKey.onDown.addOnce(this.restart, this);
+    },
+    restart: function() {
+        'use strict';
+        game.state.start('title');
+    }
+};
+
+
+game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div');
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('title', titleState);
+game.state.add('play', playState);
+game.state.add('end', endState);
+
+game.state.start('boot');
