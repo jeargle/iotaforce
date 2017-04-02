3397bcce20e6b6c26dc3835e44378bf6ccec2533
John Eargle <jeargle@gmail.com>
2017-04-02 16:59:00 -0500

    #1 #5 got player and world map loading

--- js/iotaforce.js
+++ js/iotaforce.js
@@ -22,6 +22,9 @@ loadState = {
                                  fill: '#ffffff'});
         
         // Load images
+        game.load.tilemap('world1', 'assets/world1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles', 'assets/worldTiles.png');
+        game.load.image('player', 'assets/circle-red.png');
 
         // Load sound effects
     },
@@ -58,9 +61,57 @@ playState = {
 
         this.keyboard = game.input.keyboard;
 
+        // Map
+        this.map = game.add.tilemap('world1');
+        this.map.addTilesetImage('worldTiles', 'tiles');
+        this.backgroundLayer = this.map.createLayer(0);
+        this.backgroundLayer.resizeWorld();
+        
+        // Player
+        this.player = game.add.sprite(150, game.world.height - 150, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        this.playerSpeed = 300;
+
+        game.physics.arcade.enable(this.player);
+        game.camera.follow(this.player);
+
         // Controls
+        this.cursors = game.input.keyboard.addKeys({
+            'up': Phaser.Keyboard.W,
+            'down': Phaser.Keyboard.S,
+            'left': Phaser.Keyboard.A,
+            'right': Phaser.Keyboard.D,
+            'interact': Phaser.Keyboard.SPACEBAR
+        });
     },
     update: function() {
+        'use strict';
+
+        game.physics.arcade.collide(this.player, this.walls);
+        // game.physics.arcade.collide(this.enemies, this.walls);
+        // game.physics.arcade.overlap(this.player, this.enemies,
+        //                             this.end, null, this);
+        
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
+        if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = this.playerSpeed;
+        }
+        else if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -this.playerSpeed;
+        }
+        else if (this.cursors.up.isDown) {
+            this.player.body.velocity.y = -this.playerSpeed;
+        }
+        else if (this.cursors.down.isDown) {
+            this.player.body.velocity.y = this.playerSpeed;
+        }
+
+        if (this.cursors.interact.isDown) {
+            this.interact();
+        }
+    },
+    interact: function() {
         'use strict';
 
     },
