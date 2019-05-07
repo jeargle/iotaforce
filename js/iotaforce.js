be18cce636577bd4e4f523b9beef7521e5165b75
John Eargle <jeargle@gmail.com>
2019-05-07 18:41:42 -0500

    #16 removed semicolons

--- js/iotaforce.js
+++ js/iotaforce.js
@@ -1,26 +1,26 @@
-var score, bootScene, loadScene, titleScene, playScene, endScene, game;
+var score, bootScene, loadScene, titleScene, playScene, endScene, game
 
-score = 0;
+score = 0
 
 bootScene = {
     key: 'boot',
     active: true,
     init: (config) => {
-        console.log('[BOOT] init', config);
+        console.log('[BOOT] init', config)
     },
     preload: () => {
-        console.log('[BOOT] preload');
+        console.log('[BOOT] preload')
     },
     create: function() {
-        'use strict';
+        'use strict'
 
-        game.scene.start('load');
-        game.scene.remove('boot');
+        game.scene.start('load')
+        game.scene.remove('boot')
     },
     update: () => {
-        console.log('[BOOT] update');
+        console.log('[BOOT] update')
     }
-};
+}
 
 loadScene = {
     key: 'load',
@@ -30,88 +30,88 @@ loadScene = {
     width: 320,
     height: 200,
     init: (config) => {
-        console.log('[LOAD] init', config);
+        console.log('[LOAD] init', config)
     },
     preload: function() {
-        'use strict';
-        var loadLbl;
+        'use strict'
+        var loadLbl
 
         loadLbl = this.add.text(80, 160, 'loading...',
                                 {font: '30px Courier',
-                                 fill: '#ffffff'});
+                                 fill: '#ffffff'})
 
         // Load images
-        this.load.tilemapTiledJSON('world1', 'assets/world1.json');
-        this.load.image('tiles', 'assets/worldTiles.png');
-        this.load.image('player', 'assets/circle-red.png');
+        this.load.tilemapTiledJSON('world1', 'assets/world1.json')
+        this.load.image('tiles', 'assets/worldTiles.png')
+        this.load.image('player', 'assets/circle-red.png')
 
         // Load sound effects
     },
     create: function() {
-        'use strict';
-        game.scene.start('title');
-        game.scene.remove('load');
+        'use strict'
+        game.scene.start('title')
+        game.scene.remove('load')
     },
     update: () => {
-        console.log('[LOAD] update');
+        console.log('[LOAD] update')
     }
-};
+}
 
 titleScene = {
     key: 'title',
     init: (config) => {
-        console.log('[TITLE] init', config);
+        console.log('[TITLE] init', config)
     },
     preload: () => {
-        console.log('[TITLE] preload');
+        console.log('[TITLE] preload')
     },
     create: function() {
-        'use strict';
-        var nameLbl, startLbl;
+        'use strict'
+        var nameLbl, startLbl
 
         nameLbl = this.add.text(80, 160, 'IOTA FORCE',
                                 {font: '50px Courier',
-                                 fill: '#ffffff'});
+                                 fill: '#ffffff'})
         startLbl = this.add.text(80, 240, 'press "W" to start',
                                  {font: '30px Courier',
-                                  fill: '#ffffff'});
+                                  fill: '#ffffff'})
 
-        this.input.keyboard.on('keydown_W', this.start, this);
+        this.input.keyboard.on('keydown_W', this.start, this)
     },
     update: () => {
-        console.log('[TITLE] update');
+        console.log('[TITLE] update')
     },
     extend: {
         start: function() {
-            'use strict';
-            console.log('[TITLE] start');
-            game.scene.switch('title', 'play');
+            'use strict'
+            console.log('[TITLE] start')
+            game.scene.switch('title', 'play')
         }
     }
-};
+}
 
 playScene = {
     key: 'play',
     create: function() {
-        'use strict';
-        var tileset;
+        'use strict'
+        var tileset
 
         // Map
-        this.map = this.add.tilemap('world1');
-        tileset = this.map.addTilesetImage('worldTiles', 'tiles');
-        this.backgroundLayer = this.map.createStaticLayer('backgroundLayer', tileset);
-        // this.backgroundLayer.resizeWorld();
+        this.map = this.add.tilemap('world1')
+        tileset = this.map.addTilesetImage('worldTiles', 'tiles')
+        this.backgroundLayer = this.map.createStaticLayer('backgroundLayer', tileset)
+        // this.backgroundLayer.resizeWorld()
 
         // Player
-        this.player = this.physics.add.sprite(150, 150, 'player');
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-        this.playerSpeed = 300;
+        this.player = this.physics.add.sprite(150, 150, 'player')
+        this.player.setBounce(0.2)
+        this.player.setCollideWorldBounds(true)
+        this.playerSpeed = 300
 
-        this.physics.add.collider(this.player, this.walls);
+        this.physics.add.collider(this.player, this.walls)
 
-        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
+        this.cameras.main.startFollow(this.player)
 
         // Controls
         this.cursors = this.input.keyboard.addKeys({
@@ -120,80 +120,80 @@ playScene = {
             'left': Phaser.Input.Keyboard.KeyCodes.A,
             'right': Phaser.Input.Keyboard.KeyCodes.D,
             'interact': Phaser.Input.Keyboard.KeyCodes.SPACE
-        });
+        })
 
-        // this.physics.add.collider(this.enemies, this.walls);
-        // this.physics.add.collider(this.enemies, this.enemies);
+        // this.physics.add.collider(this.enemies, this.walls)
+        // this.physics.add.collider(this.enemies, this.enemies)
         // this.physics.add.overlap(this.player, this.enemies,
-        //                          this.end, null, this);
+        //                          this.end, null, this)
 
     },
     update: function() {
-        'use strict';
+        'use strict'
 
-        console.log('[PLAY] update');
+        console.log('[PLAY] update')
 
-        this.player.body.setVelocityX(0);
-        this.player.body.setVelocityY(0);
+        this.player.body.setVelocityX(0)
+        this.player.body.setVelocityY(0)
 
         if (this.cursors.right.isDown) {
-            console.log('RIGHT');
-            this.player.body.setVelocityX(this.playerSpeed);
+            console.log('RIGHT')
+            this.player.body.setVelocityX(this.playerSpeed)
         }
         else if (this.cursors.left.isDown) {
-            console.log('LEFT');
-            this.player.body.setVelocityX(-this.playerSpeed);
+            console.log('LEFT')
+            this.player.body.setVelocityX(-this.playerSpeed)
         }
         else if (this.cursors.up.isDown) {
-            console.log('UP');
-            this.player.body.setVelocityY(-this.playerSpeed);
+            console.log('UP')
+            this.player.body.setVelocityY(-this.playerSpeed)
         }
         else if (this.cursors.down.isDown) {
-            console.log('DOWN');
-            this.player.body.setVelocityY(this.playerSpeed);
+            console.log('DOWN')
+            this.player.body.setVelocityY(this.playerSpeed)
         }
 
         if (this.cursors.interact.isDown) {
-            this.interact();
+            this.interact()
         }
     },
     extend: {
         interact: function() {
-            'use strict';
-            console.log('[PLAY] INTERACT');
+            'use strict'
+            console.log('[PLAY] INTERACT')
         },
         end: function() {
-            'use strict';
-            console.log('[PLAY] end');
+            'use strict'
+            console.log('[PLAY] end')
             game.scene.switch('play', 'end')
         }
     }
-};
+}
 
 endScene = {
     create: function() {
-        'use strict';
-        var scoreLbl, nameLbl, startLbl, wKey;
+        'use strict'
+        var scoreLbl, nameLbl, startLbl, wKey
 
         scoreLbl = this.add.text(600, 10, 'Score: ' + score,
                                  {font: '30px Courier',
-                                  fill: '#ffffff'});
+                                  fill: '#ffffff'})
         nameLbl = this.add.text(80, 160, 'YOU WIN',
                                 {font: '50px Courier',
-                                 fill: '#ffffff'});
+                                 fill: '#ffffff'})
         startLbl = this.add.text(80, 240, 'press "W" to restart',
                                  {font: '30px Courier',
-                                  fill: '#ffffff'});
+                                  fill: '#ffffff'})
 
-        this.input.keyboard.on('keydown_W', this.restart, this);
+        this.input.keyboard.on('keydown_W', this.restart, this)
     },
     extend: {
         restart: function() {
-            'use strict';
+            'use strict'
             game.scene.switch('end', 'title')
         }
     }
-};
+}
 
 
 const gameConfig = {
@@ -205,9 +205,6 @@ const gameConfig = {
         default: 'arcade',
         arcade: {
             debug: true,
-            gravity: {
-                y: 600
-            },
             height: 775,
             width: 1600,
             x: 0,
@@ -215,7 +212,7 @@ const gameConfig = {
         }
     },
     scene: [bootScene, loadScene, titleScene, playScene, endScene]
-};
+}
 
-game = new Phaser.Game(gameConfig);
-game.scene.start('boot', { someData: '...arbitrary data' });
+game = new Phaser.Game(gameConfig)
+game.scene.start('boot', { someData: '...arbitrary data' })
